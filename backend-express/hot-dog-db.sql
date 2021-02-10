d4d72a7fb9e2977c189719253943e3b0e853fcdd
Caleb Mcolin <Caleb.McOlin@seattlecolleges.edu>
2021-02-09 19:22:34 -0800

    Formated the SQL script

--- backend-express/hot-dog-db.sql
+++ backend-express/hot-dog-db.sql
@@ -6,11 +6,11 @@ USE `hot-dog-db`;
 -- Table Employee
 -- -----------------------------------------------------
 CREATE TABLE Employee (
-  employee_id 				INT 		      NOT NULL,
-  employee_first_name VARCHAR(45) 	NOT NULL,
-  employee_last_name 	VARCHAR(45)   NOT NULL,
-  employee_phone 			INT 		    	NOT NULL,
-  employee_salary			INT 		    	NOT NULL,
+  employee_id INT NOT NULL,
+  employee_first_name VARCHAR(45) NOT NULL,
+  employee_last_name VARCHAR(45) NOT NULL,
+  employee_phone INT NOT NULL,
+  employee_salary INT NOT NULL,
   PRIMARY KEY (employee_id));
 
 
@@ -18,12 +18,12 @@ CREATE TABLE Employee (
 -- Table Address
 -- -----------------------------------------------------
 CREATE TABLE Address (
-  address_id 	  	INT 		      NOT NULL,
-  address_street 	VARCHAR(45)		NOT NULL,
-  address_suite 	INT 			    NOT NULL,
-  address_zip 		INT 		    	NOT NULL,
-  address_city 		VARCHAR(45) 	NOT NULL,
-  address_state 	VARCHAR(45) 	NOT NULL,
+  address_id INT NOT NULL,
+  address_street VARCHAR(45) NOT NULL,
+  address_suite INT NOT NULL,
+  address_zip INT NOT NULL,
+  address_city VARCHAR(45) NOT NULL,
+  address_state VARCHAR(45) NOT NULL,
   PRIMARY KEY (address_id));
 
 
@@ -31,9 +31,9 @@ CREATE TABLE Address (
 -- Table Vendor
 -- -----------------------------------------------------
 CREATE TABLE Vendor (
-  vendor_id 	  INT 	NOT NULL,
-  employee_id   INT 	NOT NULL,
-  address_id 	  INT 	NOT NULL,
+  vendor_id INT NOT NULL,
+  employee_id INT NOT NULL,
+  address_id INT NOT NULL,
   PRIMARY KEY (vendor_id),
   INDEX fk_Vendor_Employee_idx (employee_id ASC) VISIBLE,
   INDEX fk_Vendor_Address_idx (address_id ASC) VISIBLE,
@@ -49,8 +49,8 @@ CREATE TABLE Vendor (
 -- Table Admin
 -- -----------------------------------------------------
 CREATE TABLE Admin (
-  admin_id 		  INT		NOT NULL,
-  employee_id 	INT 	NOT NULL,
+  admin_id INTNOT NULL,
+  employee_id INT NOT NULL,
   PRIMARY KEY (admin_id),
   INDEX fk_Admin_Employee_idx (employee_id ASC) VISIBLE,
   CONSTRAINT fk_Admin_Employee FOREIGN KEY (employee_id) REFERENCES Employee (employee_id)
@@ -62,10 +62,10 @@ CREATE TABLE Admin (
 -- Table Inventory
 -- -----------------------------------------------------
 CREATE TABLE Inventory (
-  inventory_id 		INT 			    NOT NULL,
-  inventory_type 	VARCHAR(45)   NOT NULL,
-  inventory_price INT 			    NOT NULL,
-  inventory_cost 	INT 			    NOT NULL,
+  inventory_id INT NOT NULL,
+  inventory_type VARCHAR(45) NOT NULL,
+  inventory_price INT NOT NULL,
+  inventory_cost INT NOT NULL,
   PRIMARY KEY (inventory_id));
 
 
@@ -73,10 +73,10 @@ CREATE TABLE Inventory (
 -- Table Customer
 -- -----------------------------------------------------
 CREATE TABLE Customer (
-  cust_id 			    INT 			    NOT NULL,
-  address_id 		    INT 			    NOT NULL,
-  cust_first_name 	VARCHAR(45) 	NOT NULL,
-  cust_last_name    VARCHAR(45) 	NOT NULL,
+  cust_id INT NOT NULL,
+  address_id INT NOT NULL,
+  cust_first_name VARCHAR(45) NOT NULL,
+  cust_last_name VARCHAR(45) NOT NULL,
   PRIMARY KEY (cust_id),
   INDEX fk_Customer_Address_idx (address_id ASC) VISIBLE,
   CONSTRAINT fk_Customer_Address FOREIGN KEY (address_id) REFERENCES Address (address_id)
@@ -88,11 +88,11 @@ CREATE TABLE Customer (
 -- Table Order
 -- -----------------------------------------------------
 CREATE TABLE `Order` (
-  order_id 			    INT 			    NOT NULL,
-  vendor_id 		    INT 			    NOT NULL,
-  cust_id 			    INT 			    NOT NULL,
-  order_price 		  VARCHAR(45)   NOT NULL,
-  order_date_time   DATETIME 		   NOT NULL,
+  order_id INT NOT NULL,
+  vendor_id INT NOT NULL,
+  cust_id INT NOT NULL,
+  order_price VARCHAR(45) NOT NULL,
+  order_date_time DATETIME NOT NULL,
   PRIMARY KEY (order_id),
   INDEX fk_Order_Vendor_idx (vendor_id ASC) VISIBLE,
   INDEX fk_Order_Customer_idx (cust_id ASC) VISIBLE,
@@ -108,10 +108,10 @@ CREATE TABLE `Order` (
 -- Table Payment
 -- -----------------------------------------------------
 CREATE TABLE Payment (
-  payment_id 		    	INT 	  	NOT NULL,
-  cust_id 				    INT 	   	NOT NULL,
-  payment_price 		  INT 		  NOT NULL,
-  payment_date_time 	DATETIME 	NOT NULL,
+  payment_id INT NOT NULL,
+  cust_id INT NOT NULL,
+  payment_price INT NOT NULL,
+  payment_date_time DATETIME NOT NULL,
   PRIMARY KEY (payment_id),
   INDEX fk_Payment_Customer_idx (cust_id ASC) VISIBLE,
   CONSTRAINT fk_Payment_Customer FOREIGN KEY (cust_id) REFERENCES Customer (cust_id)
@@ -123,9 +123,9 @@ CREATE TABLE Payment (
 -- Table Vendor_has_Inventory
 -- -----------------------------------------------------
 CREATE TABLE Vendor_has_Inventory (
-  vendor_id 		    INT 		  NOT NULL,
-  inventory_id 		  INT 		  NOT NULL,
-  inventory_avail 	TINYINT 	NOT NULL,
+  vendor_id INT NOT NULL,
+  inventory_id INT NOT NULL,
+  inventory_avail TINYINT NOT NULL,
   PRIMARY KEY (vendor_id, inventory_id),
   INDEX fk_Vendor_has_Inventory_Inventory_idx (inventory_id ASC) VISIBLE,
   INDEX fk_Vendor_has_Inventory_Vendor_idx (vendor_id ASC) VISIBLE,
