540d15e17c2dba92a63921670c6a01e477a03461
Scott H <zenwattage@gmail.com>
2021-02-11 08:21:41 -0800

    adding signup and signin routes

--- backend-express/routes/index.js
+++ backend-express/routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+//SIGNUP ROUTE
+router.post('/signup', (req, res, next) => {
+  //functionality to signup to the app goes here
+});
+
+//SIGNIN ROUTE
+router.post('/signin', function (req, res, next) {
+  //functionalityh to signin to the app goes here
 });
 
 module.exports = router;
