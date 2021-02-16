399734b7aaa3926023d0dc2695459cb985ac20ed
Payam Taherirostami <payamtaherirostami@gmail.com>
2021-02-16 07:07:04 -0800

    issues with FKs

--- backend-express/routes/vendor.js
+++ backend-express/routes/vendor.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+const mysql = require('mysql2');
+let myCredentials = require('../../../dbCreds.json');
+const connection = mysql.createConnection(myCredentials);
+//const vendorRouter= require('./routes/vendor');
+//app.use('/vendor', vendorRouter);
 //************************************************************* */
 // router.get('/', (req, res, next) => {
 //     res.send('Hello dear vendor!');
@@ -8,61 +12,71 @@ var router = express.Router();
 
 //CREATE//
 router.post('/',(req, res, next) => {
-   let mySQLQuery = `INSERT INTO vendor (vendor_id) VALUES (${req.body.vendor_id})`;
-   Connection.connect();
-   Connection.Query(mySQLQuery, (error, results) => {
+   let mySQLQuery3 =  `INSERT INTO Address ( address_id, address_street, address_suite, address_zip, address_city,address_state) VALUES    (${req.body.address_id},'${req.body.address_street}',${req.body.address_suite},${req.body.address_zip},'${req.body.city}','${req.body.address_state}')`;
+   let mySQLQuery2 =  `INSERT INTO Employee (employee_id, employee_first_name, employee_last_name, employee_phone, employee_salary) VALUES (${req.body.employee_id},'${req.body.employee_first_name}','${req.body.employee_last_name}',${req.body.employee_phone},${req.body.employee_salary})`;
+   let mySQLQuery1 =  `INSERT INTO Vendor (vendor_id, employee_id, address_id) VALUES (${req.body.vendor_id},${req.body.employee_id},${req.body.address_id})`;
+   //connection.connect();
+   connection.query(mySQLQuery2, (error, results) => {
       if(error) {
-         res.send(500);
-      }
-      res.status(201).send(results);
+         console.log(error);
+         res.sendStatus(500);
+      }else {
+         res.send(results);
+      } 
       })
-   Connection.end();
+   //connection.end();
 });
 
-///READ//
+///Returns all the data in this route//
  router.get('/',(req, res, next) => {
-    let mySQLQuery;
-if (req.query.vendor_id){
-    mySQLQuery= `SELECT * FROM vendor WHERE vendor_id = '${req.query.vendor_id}'`;
+    let mySQLQuery=  `SELECT * FROM vendor WHERE vendor_id = '${req.params.id}'`;
+if (req.params.id){
+    mySQLQuery= `SELECT * FROM vendor WHERE vendor_id = '${req.params.id}'`;
 }else{
     mySQLQuery= `SELECT * FROM vendor`;
 }
-   Connection.connect();
-   Connection.Query(mySQLQuery, (error, results) => {
+   //connection.connect();
+   connection.query(mySQLQuery, (error, results) => {
       if(error) {
-         res.send(500);
-      }
-      res.send(results);
+         res.sendStatus(500);
+      }else {
+         res.send(results);
+      } 
       })
-   Connection.end();
+   //connection.end();
 });
 
-//"/value"
-router.get('/:vendor_id', function(req, res,next){     
-   let mySQLQuery = `SELECT * FROM vendor WHERE vendor_id= '${req.params.vendor_id}'`;
-   Connection.connect();
-   Connection.Query(mySQLQuery, (error, results) => {
-      if(error) {
-         res.send(500);
-      }
-      res.send(results);
-      })
-   Connection.end();
+// Returns /value"
+router.get('/:id',(req, res, next) => {
+   let mySQLQuery=  `SELECT * FROM vendor WHERE vendor_id = '${req.params.id}'`;
+if (req.params.id){
+   mySQLQuery= `SELECT * FROM vendor WHERE vendor_id = '${req.params.id}'`;
+}else{
+   mySQLQuery= `SELECT * FROM vendor`;
+}
+  //connection.connect();
+  connection.query(mySQLQuery, (error, results) => {
+     if(error) {
+        res.sendStatus(500);
+     }else {
+        res.send(results);
+     }
+     })
+  //connection.end();
 });
-
 
  //UPDATE
-router.put('/:vendor_id',(req, res, next) =>{
-   let mySQLQuery = `UPDATE vendor SET employee_id = ${req.body.emploee_id} WHERE vendor_id = ${req.params.vendor_id}`;
-   Connection.connect();
-   Connection.Query(mySQLQuery, (error, results) => {
-      if(error) {
-         res.send(500);
-      }
-      res.send(results);
-      })
-   Connection.end();
-});
+// router.put('/:vendor_id',(req, res, next) =>{
+//    let mySQLQuery = `UPDATE vendor SET employee_id = ${req.body.emploee_id} WHERE vendor_id = ${req.params.vendor_id}`;
+//    Connection.connect();
+//    Connection.Query(mySQLQuery, (error, results) => {
+//       if(error) {
+//          res.send(500);
+//       }
+//       res.send(results);
+//       })
+//    Connection.end();
+// });
 
 // **********************    for loop   *********************** */
 // router.get('/:id', function (req, res, next) {
@@ -78,7 +92,16 @@ router.put('/:vendor_id',(req, res, next) =>{
 // });
 
 
+// connection.query(
+//   'SELECT * FROM `Employee`',
+//   function (err, results) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(results); // results contains rows returned by server
 
+//   }
+// );
 
 
 module.exports = router;
