dbac00e77c0a624747a0156a61646c2a77fbc5a8
Payam Taherirostami <payamtaherirostami@gmail.com>
2021-02-27 13:55:17 -0800

    adding components

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,42 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import OrderDetails from './components/OrderDetails';
+import OrdersList from './components/OrdersList';
 import './App.css';
+import React, { Component } from 'react';
+import { render } from "react-dom";
+import Orders from './components/Orders';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Route }  from 'react-router';
+import { useState } from 'react';
 
-class App extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = { apiResponse: 'THIS FRONTEND CRAP WORKS' }
-  }
+class App extends React.Component{
+  
+ 
+  constructor(props){
+  super(props);
 
-  callAPI() {
-    fetch("http://localhost:9000/testAPI")
-      .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res }));
-  }
-
-  componentWillMount() {
-    this.callAPI();
-  }
-
-  render() {
-    return (
-      <div className="App" >
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className="App-intro">{this.state.apiResponse}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
-      </div>
-    );
+  this.state = {
+    orders: [],
+    areOrdersFetched: false
+    //,
+    //  activeOrder:{}
   }
 }
+componentDidMount() {
+  let myTestDataAPi = fetch("http://localhost:9000/vendor")
+    .then((results) => {
+      return results.json();
+    }).then((myJson) => {
+      console.log("IN JSON promise resolution", myJson);
+      this.setState({
+        orders: myJson,
+        areOrdersFetched: true
+      });
+    })
+}
+  render() {
+    ///////////////////////////////////////////////////////////////////
+    // const DeleteOrder = (props) =>  {
+    //   OrdersList.filter((order) => order.vendor_id!== props );
+    //   console.log('deleted');
+    // } 
+    //////////////////////////////////////////////////////////////////
 
+    let myOrderList=this.state.areOrdersFetched
+    ?   <OrdersList listOfOrders={ this.state.orders } 
+    //onDelete={onClick}
+     />   
+    : <h2>Orders are loading!</h2>
+    let myOrderDetails= !!this.state.activeOrder
+    ? <OrderDetails   AddressId={this.state.activeOrder.address_id}/>
+    : <h4>Select an Order</h4>
+
+  return (
+    <div className='App'> 
+      <header className="App-header">
+      <Orders />     
+       
+        { myOrderList }
+      {/* <OrderListItem/> */}
+       { myOrderDetails }
+      </header>
+    </div> 
+     );
+  }
+}
 export default App;
---
+++ frontend-react/src/components/OrderListItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Buttons from './Buttons' ;
+// import Orders from './Orders';
+
+class OrderListItem extends React.Component { 
+    render() {
+        return <div className='data'> 
+           <ul id= "ords">
+            <li>vendor-id: {this.props.order.vendor_id}  
+            <Buttons color='red' text='Complete' 
+            // onClick={onClick}
+            /> </li>
+            <h4>employee-id {this.props.order.employee_id} </h4>  {/*date of order?*/}
+            </ul> 
+        </div>;
+    }   
+}
+export default OrderListItem;
