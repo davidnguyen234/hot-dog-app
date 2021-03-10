c781789e83b6682987e381ebe895aa0c2c3c44c5
Caleb Mcolin <Caleb.McOlin@seattlecolleges.edu>
2021-03-09 18:11:52 -0800

    fixed import. Removed "<body>"

--- frontend-react/src/components/vendor/Orders/OrdersList.js
+++ frontend-react/src/components/vendor/Orders/OrdersList.js
@@ -3,20 +3,26 @@ import OrderListItem from './OrderListItem';
 
 function OrdersList(props) {
     // array of order data
-    const myOrderListItemArray = props.listOfOrders.map((orderLItem) =>{
+    const myOrderListItemArray = props.listOfOrders.map((orderLItem) => {
 
-    return <div>< OrderListItem 
-                key={orderLItem.id}
-                order={orderLItem} 
-                activeOrderId= {props.activeOrderId}
-                myClickHandler={props.myClickHandler} 
-                deleteHandler={props.deleteHandler}
-               /> 
-    </div>
+        return (
+            <div>
+                < OrderListItem
+                    key={orderLItem.id}
+                    order={orderLItem}
+                    activeOrderId={props.activeOrderId}
+                    myClickHandler={props.myClickHandler}
+                    deleteHandler={props.deleteHandler}
+                />
+            </div>
+        )
     });
-   
+
     // returning the array in a div
-    return <div> {myOrderListItemArray}
-    </div>;
+    return (
+        <div>
+            {myOrderListItemArray}
+        </div>
+    );
 }
 export default OrdersList;
--- frontend-react/src/components/vendor/VendorNav.js
+++ frontend-react/src/components/vendor/VendorNav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../css/vendorNav.css';
-import Orders from './Orders/Orders';
+import Orders from './Orders/OrderMain';
 import Items from './Items';
 import VendorDropDown from './VendorDropDown';
 import {
