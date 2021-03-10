42c9ccb3d3191ce86192b36d76d222cc56e9b75e
Caleb Mcolin <Caleb.McOlin@seattlecolleges.edu>
2021-03-09 19:26:43 -0800

    fix to mistake merge

--- frontend-react/src/components/vendor/VendorNav.js
+++ frontend-react/src/components/vendor/VendorNav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../css/vendorNav.css';
 import Orders from './Orders/OrderMain';
-import Items from './Items';
+import Items from './Items/Items';
 import Address from './Address/Address';
 import VendorDropDown from './VendorDropDown';
 import {
