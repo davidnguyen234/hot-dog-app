8a89519ea2e9ece94f0bbb8f63ceb41a7b6d137e
Caleb Mcolin <Caleb.McOlin@seattlecolleges.edu>
2021-03-11 14:30:38 -0800

    added a toggle component that will update the vendor table when clicked

---
+++ frontend-react/src/components/vendor/Address/Toggle.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+
+class Toggle extends React.Component {
+    render() {
+        let buttonText;
+        const style = {
+            width: '340px',
+            border: '1px #AAA solid',
+            padding: '10px',
+            backgroundColor: '#000000' // RED
+        }
+
+        if (this.props.vendorAvail === 1) {
+            buttonText = "Open for Business"
+            style.backgroundColor = '#32CD32'; // GREEN
+        } else if (this.props.vendorAvail === 0) {
+            buttonText = "Closed for Business"
+            style.backgroundColor = '#FF0000'; // GREEN
+        }
+        return (
+            <button style={style} onClick={(e) => this.props.myClickHandler(e)}>
+                {buttonText}
+            </button>
+        );
+    }
+}
+export default Toggle;
