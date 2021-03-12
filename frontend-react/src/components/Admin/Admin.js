2f2b9ccefb4b95cd3971e2a9e5fe2b0b30834735
Caleb Mcolin <Caleb.McOlin@seattlecolleges.edu>
2021-03-12 13:36:39 -0800

    connected admin to nav. Get call shows all elements in desired tables

--- frontend-react/src/components/Admin/Admin.js
+++ frontend-react/src/components/Admin/Admin.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Axios from 'axios';
+import Popup from 'reactjs-popup';
 
 class Admin extends React.Component {
     constructor(props) {
@@ -13,44 +14,102 @@ class Admin extends React.Component {
     }
 
     componentDidMount() {
-        this.getItem();
-        this.getVendor();
-        this.getOrders();
-        this.getAddress();
+        this.getTable("inventory");
+        this.getTable("vendor");
+        this.getTable("orders");
+        this.getTable("address");
     }
 
     // GET CALLS //
-    // Adds all the inventory items to the itemList
-    getItem() {
-        Axios.get("http://localhost:9000/items/").then(res => {
-            this.setState({ itemList: [...res.data] })
+    // Adds all the items from the given table name, and populates the respected list
+    getTable(table) {
+        Axios.get("http://localhost:9000/admin/" + table + "/").then(res => {
+            switch (table) {
+                case "inventory":
+                    this.setState({ itemList: [...res.data] });
+                    break;
+                case "orders":
+                    this.setState({ ordersList: [...res.data] })
+                    break;
+                case "vendor":
+                    this.setState({ vendorList: [...res.data] })
+                    break;
+                default:
+                    this.setState({ addressList: [...res.data] })
+                    break;
+            }
         });
     }
 
-    // Adds each vendor to the vendorList
-    getVendor() {
-        Axios.get("http://localhost:9000/vendor/").then(res => {
-            this.setState({ vendorList: [...res.data] })
-        });
+    render() {
+        return (
+            <div className="adminPage">
+
+                <div id="inventoryList">
+                    Inventory:
+                    {this.state.itemList.map(item => (
+                    <Popup
+                        trigger={<div id="InventoryItem"> {item.inventory_type} </div>}
+                        on="hover"
+                        mouseLeaveDelay={300}
+                        mouseEnterDelay={300}
+                    >
+                        <div id="inventoryOptions">
+                            Options: [Edit] [Delete]
+                        </div>
+                    </Popup>
+                ))}
+                </div>
+
+                <div id="ordersList">
+                    Orders:
+                    {this.state.ordersList.map(order => (
+                    <Popup
+                        trigger={<div id="orderItem"> {order.orders_id} </div>}
+                        on="hover"
+                        mouseLeaveDelay={300}
+                        mouseEnterDelay={300}
+                    >
+                        <div id="orderOptions">
+                            Options: [Edit] [Delete]
+                        </div>
+                    </Popup>
+                ))}
+                </div>
+
+                <div id="vendorList">
+                    Vendors:
+                    {this.state.vendorList.map(vendor => (
+                    <Popup
+                        trigger={<div id="orderItem"> {vendor.vendor_id} </div>}
+                        on="hover"
+                        mouseLeaveDelay={300}
+                        mouseEnterDelay={300}
+                    >
+                        <div id="orderOptions">
+                            Options: [Edit] [Delete]
+                        </div>
+                    </Popup>
+                ))}
+                </div>
+
+                <div id="addressList">
+                    Addresses:
+                    {this.state.addressList.map(address => (
+                    <Popup
+                        trigger={<div id="orderItem"> {address.address_id} </div>}
+                        on="hover"
+                        mouseLeaveDelay={300}
+                        mouseEnterDelay={300}
+                    >
+                        <div id="orderOptions">
+                            Options: Not avalible
+                        </div>
+                    </Popup>
+                ))}
+                </div>
+            </div>
+        );
     }
-
-    // Adds every order to the orderList
-    getOrders() {
-        Axios.get("http://localhost:9000/orders/").then(res => {
-            this.setState({ ordersList: [...res.data] })
-        });
-    }
-
-    // Adds every address to the addressList
-    getAddress() {
-        Axios.get("http://localhost:9000/address/").then(res => {
-            this.setState({ addressList: [...res.data] })
-        });
-    }
-
-
-
-
-
 }
 export default Admin;
--- frontend-react/src/components/Nav.js
+++ frontend-react/src/components/Nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Map from './Maps';
 import Menu from './Menu/Menu';
 import Vendor from './vendor/VendorNav';
+import Admin from './Admin/Admin';
 import '../css/Nav.css';
 import {
     BrowserRouter as Router,
@@ -19,7 +20,7 @@ export default function Nav() {
         <Router>
             <div className="vendor-nav">
                 <ul className="nav-links">
-                <Link style={nav_links_style} to="/map">
+                    <Link style={nav_links_style} to="/map">
                         <li>Map</li>
                     </Link>
                     <Link style={nav_links_style} to="/menu">
@@ -28,14 +29,16 @@ export default function Nav() {
                     <Link style={nav_links_style} to="/vendor">
                         <li>Vendor</li>
                     </Link>
-                    
-                   
+                    <Link style={nav_links_style} to="/admin">
+                        <li>Admin</li>
+                    </Link>
                 </ul>
             </div>
             <Switch>
                 <Route path="/map" component={Map} exact />
                 <Route path="/menu" component={Menu} exact />
                 <Route path="/vendor" component={Vendor} exact />
+                <Route path="/admin" component={Admin} exact />
             </Switch>
         </Router>
     );
