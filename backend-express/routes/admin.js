929d2147492b0d64ceb16443658a6596e499ee9f
Caleb Mcolin <Caleb.McOlin@seattlecolleges.edu>
2021-03-17 17:00:08 -0700

    adds to the "vendor" and "vendor_has_inventory" tables

--- backend-express/routes/admin.js
+++ backend-express/routes/admin.js
@@ -18,18 +18,31 @@ router.get('/:tableName', function (req, res, next) {
 });
 
 // POST ROUTES
-// adds a new vendor with the given phone and id
+// Adds a new vendor with the given phone and id adds the all the inventory to the newly added vendor (Inserts into "vendor" and "vendor_has_inventory")
 router.post('/vendor/:venPhone/:empId', function (req, res, next) {
-    db.query(
-        `INSERT INTO vendor VALUES (DEFAULT, ${req.params.venPhone}, '12:00 PM - 8:00 PM', ${req.params.empId}, 4, 0)`,
-        function (err, results) {
-            if (!err) {
-                res.send(results);
-            } else {
-                console.log(err);
-            }
+   var insert1 = `INSERT INTO vendor VALUES (DEFAULT, '${req.params.venPhone}', '12:00 PM - 8:00 PM', ${req.params.empId}, 4, 0)`;
+   var insert2 = `INSERT INTO Vendor_has_Inventory (vendor_id, inventory_id) SELECT v.vendor_id, i.inventory_id FROM (SELECT vendor_id FROM vendor ORDER BY vendor_id DESC LIMIT 1) AS v CROSS JOIN (SELECT inventory_id FROM inventory) AS i;`;
+   async.parallel([
+    function (parallel_done) {
+        db.query(insert1, {}, function (err, results) {
+            if (err) return parallel_done(err);
+            parallel_done();
+        });
+    },
+    function (parallel_done) {
+        db.query(insert2, {}, function (err, results) {
+            if (err) return parallel_done(err);
+            parallel_done();
+        });
+    }
+],
+    function (err, results) {
+        if (!err) {
+            res.send(results);
+        } else {
+            console.log(err);
         }
-    );
+    });
 });
 
 // Add a new item to the Inventory table, then adds the new item to each vendor in the "vendor_has_inventory" table
@@ -58,4 +71,7 @@ router.post('/item/:itemName/:itemPrice/:itemCost', function (req, res, next) {
             }
         });
 });
+
+
+
 module.exports = router;
